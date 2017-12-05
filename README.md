ceaedb39bf290a90d88af6bfaa7ad441393b0706
Paul Buzuloiu <pbuzu025@uottawa.ca>
2017-12-05 03:08:34 -0500

    styling and scaling

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # mdesktop
 Generating MacOS desktop wallpapers from Markdown with ruby
 
-![](https://user-images.githubusercontent.com/20449016/33522796-dbf397c8-d7c3-11e7-93a3-0a6aba7a3b3b.png)
 
-This is a **total** WIP right now, there are scaling issues with the markdown, and adding too many lines makes it run off the page.
+This is a **total** WIP right now, there are scaling issues with the markdown.
 
 The idea is to be able to put whatever you want on the desktop:
 - cheat sheets for programming languages
@@ -16,8 +15,4 @@ The idea is to be able to put whatever you want on the desktop:
 2. run `bundle install`
 3. edit the `EDIT_ME.md` file to contain whatever you want
 4. run `ruby ./set_background.rb`
-
-## Issues
-
-- sometimes the scripts that sets the wallpaper has a problem changing the wallpaper. If this happens you can set the wallpaper manually to the `/lib/wallpaper.jpg` file.
-  - sometimes this issues is resolved by switching to a different default desktop and then running the script again.
+5. you can add columns with the `>` character instead of blockquotes
--- set_background.rb
+++ set_background.rb
@@ -1,5 +1,6 @@
 require 'imgkit'
 require 'redcarpet'
+require 'fastimage'
 
 FileUtils.rm_rf(Dir.glob('./lib/temp/*'))
 
@@ -16,11 +17,29 @@ file_name = Array.new(10){rand(36).to_s(36)}.join
 #parsing user's markdown to HTML
 mdown = File.read('./EDIT_ME.md')
 html = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true).render(mdown)
-File.write('./converted.html', html)
+
+
+split_html =  html.split('<blockquote>')
+split_html.each do |item|
+  item.prepend('<div class="column">')
+  item << '</div>'
+end
+html = split_html.join(" ")
+html.prepend('<div id="row">')
+html << '</div>'
+
+puts html
 
 puts "generating Image from HTML"
 # run `wkhtmltoimage --extended-help` for a full list of options
-kit = IMGKit.new(File.new('./converted.html'),:height => screen_height, :width => screen_width)
+kit  = IMGKit.new(html)
+file = kit.to_file("./lib/temp/unscaled.jpg")
+size = FastImage.size("./lib/temp/unscaled.jpg")
+scale = (screen_height.to_f/size[1].to_f)
+
+kit = IMGKit.new(html,:height => screen_height, :width => screen_width, :zoom => scale, :quality=>10000)
+kit.stylesheets << './lib/style.scss'
+kit.javascripts << './lib/style.js'
 
 file = kit.to_file("./lib/temp/#{file_name}.jpg")
 
